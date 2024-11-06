f6d6b7ced029219e00c4f51e07dffe0aa4284862
SlanSaf <slansaf@yandex.ru>
2024-11-06 14:57:12 +0300

    Выполнено домашнее задаине к урокам 1-2

---
+++ java_script.js
@@ -0,0 +1,13 @@
+let degreesCelsius = 10;
+
+let degreesFahrenheit = ((9/5) * degreesCelsius) + 32;
+
+console.log(`Градусы Цельсия: ${degreesCelsius} градусов`);
+console.log(`Градусы Фарингейта: ${degreesFahrenheit} градусов`);
+
+
+let name = 'Ruslan';
+
+let admin = name;
+
+console.log(`Имя админа: ${admin}`);
