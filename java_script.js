94ae81016e27eacc3ddfe140596abfde4f26abc3
SlanSaf <slansaf@yandex.ru>
2024-11-07 20:26:53 +0300

    Выполнено домашне задание к занятиям 3-4

--- java_script.js
+++ java_script.js
@@ -1,13 +1,33 @@
-let degreesCelsius = 10;
+// let num1 = Number(prompt('Введите первое число'));
+// let num2 = Number(prompt('Введите второе число'));
 
-let degreesFahrenheit = ((9/5) * degreesCelsius) + 32;
-
-console.log(`Градусы Цельсия: ${degreesCelsius} градусов`);
-console.log(`Градусы Фарингейта: ${degreesFahrenheit} градусов`);
+// (num1 <= num2 && num2 >= 3)? console.log('Условия выполнены')  : console.log('Условия не выполнены');
 
 
-let name = 'Ruslan';
+// let test = true;
+// if (test === true) {
+// console.log('+++');
+// } else {
+// console.log('---');
+// }
 
-let admin = name;
+// (test === true)? console.log('+++') : console.log('---');;
 
-console.log(`Имя админа: ${admin}`);
+// let day = Number(prompt('Введите число от 1 до 31'));
+
+// if(day >= 1 && day <= 10){
+// 	console.log('Первая декада месяца');
+// } else if (day >= 11 && day <= 20) {
+// 	console.log('Вторая декада месяца');
+// } else if (day >= 21 && day <= 31){
+// 	console.log('Третяя декада месяца');
+// } else {
+// 	console.log('Введено неверное значение числа');
+// }
+
+let num = Number(prompt('Введите число'));
+
+console.log(`Вы ввели число: ${num}`);
+console.log(`Единицы: ${num%10}`);
+console.log(`Десятки: ${Math.floor(num%100/10)}`);
+console.log(`Сотни: ${Math.floor(num%1000/100)}`);
